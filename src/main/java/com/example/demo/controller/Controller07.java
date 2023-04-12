3772f021a4809d05efc2ff107937464e089863a2
soheeG <121710859+soheeG@users.noreply.github.com>
2023-04-12 15:34:52 +0900

    20230412

--- src/main/java/com/example/demo/controller/Controller07.java
+++ src/main/java/com/example/demo/controller/Controller07.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.*;
+
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.domain.*;
 
 import jakarta.servlet.http.*;
 
@@ -38,5 +42,63 @@ public class Controller07 {
 		
 		model.addAttribute("adress", "seoul");
 		
+	}
+	
+	@RequestMapping("link4")
+	public void method4(Model model) {
+		model.addAttribute("list", List.of("java", "spring"));
+	}
+	
+	@RequestMapping("link5")
+	public void method5(Model model) {
+		Map<String, Object> obj = new HashMap<>();
+		obj.put("address", "seoul");
+		obj.put("age", "30");
+		obj.put("email", "son@gmail.com");
+		
+		model.addAttribute("myMap", obj);
+	}
+	
+	@RequestMapping("link6")
+	public void method6(Model model) {
+		model.addAttribute("name", "이한나");
+		model.addAttribute("job", "매니저");
+		model.addAttribute("hobby", List.of("영화", "독서", "음악"));
+	}
+	
+	@RequestMapping("link7")
+	public void method7(Model model) {
+		model.addAttribute("age", "30");
+		model.addAttribute("country", "Korea");
+		model.addAttribute("movieList", new String[] {"길복순", "스즈메의 문단속", "슬램덩크"});
+	}
+	
+	@RequestMapping("link8")
+	public void method8(Model model) {
+		Dto01 o1 = new Dto01();
+		o1.setName("박지성");
+		o1.setAge(40);
+		
+		model.addAttribute("value", o1);
+	}
+	
+	@RequestMapping("link9")
+	public void method9(Model model) {
+		Dto02 o2 = new Dto02();
+		
+		o2.setModel("genesis");
+		o2.setPrice(9500);
+		o2.setCompany("hyundai");
+		
+		model.addAttribute("value", o2);
+	}
+	
+	@RequestMapping("link10") 
+	public void method10(Model model) {
+		Dto04 obj = new Dto04();
+		obj.setName("송태섭");
+		obj.setAge(20);
+		
+		model.addAttribute("val", obj);
 	}
 }
