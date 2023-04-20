706059221329cb04d69cdfc0404bf2fd5766d627
soheeG <121710859+soheeG@users.noreply.github.com>
2023-04-20 11:04:11 +0900

    20230420

--- src/main/java/com/example/demo/controller/Controller19.java
+++ src/main/java/com/example/demo/controller/Controller19.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.sql.*;
+import java.time.*;
 
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
@@ -127,6 +128,115 @@ public class Controller19 {
 			
 			System.out.println(cnt + "개 행 입력됨");
 			
+		}
+	}
+	
+	@RequestMapping("link6")
+	public void method6() {
+		// forward to form
+
+	}
+	
+	@RequestMapping("link7")
+	public void method7(
+			@RequestParam("name")
+			String name,
+			@RequestParam("age")
+			String age,
+			@RequestParam("price")
+			String price,
+			@RequestParam("birth")
+			String birth,
+			@RequestParam("inserted")
+			String inserted
+			) throws Exception {
+		
+		String sql = """
+				INSERT INTO MyTable32 
+					(Name, Age, Price, Birth, Inserted)
+				VALUES (?, ?, ?, ?, ?)
+				""";
+		
+		try (
+				Connection con = DriverManager.getConnection(url, username, password);
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				) {
+		pstmt.setString(1, name);
+		pstmt.setString(2, age);
+		pstmt.setString(3, price);
+		pstmt.setString(4, birth);
+		pstmt.setString(5, inserted);
+		
+		int cnt = pstmt.executeUpdate();
+		System.out.println(cnt + "개 행 입력됨");
+		}
+	}
+	
+	@RequestMapping("link8")
+	public void method8(
+			@RequestParam("name") String name,
+			@RequestParam("age") Integer age,
+			@RequestParam("price") Double price,
+			@RequestParam("birth") LocalDate birth,
+			@RequestParam("inserted") LocalDateTime inserted
+			) throws Exception {
+		
+		String sql = """
+				INSERT INTO MyTable32
+					(Name, Age, Price, Birth, Inserted)
+					VALUES (?, ?, ?, ?, ?)
+				""";
+		
+		try (
+				Connection con = DriverManager.getConnection(url, username, password);
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				) {
+			
+		pstmt.setString(1, name);
+		pstmt.setInt(2, age);
+		pstmt.setDouble(3, price);
+		pstmt.setDate(4, Date.valueOf(birth));
+		pstmt.setTimestamp(5, Timestamp.valueOf(inserted));
+		int cnt = pstmt.executeUpdate();
+		System.out.println(cnt + "개 행 입력됨");
+		}
+	}
+	
+	
+	@RequestMapping("link9")
+	public void method9() {
+		
+	}
+	
+	@RequestMapping("link10")
+	public void method10(
+			@RequestParam("name") String name,
+			@RequestParam("published") LocalDate published,
+			@RequestParam("price") Integer price,
+			@RequestParam("updated") LocalDate updated,
+			@RequestParam("weight") Double weight
+			
+			) throws Exception {
+		
+		String sql = """
+				INSERT INTO MyTable33 
+					(Name, Age, Price, Updated, Weight)
+				VALUES (?, ?, ?, ?, ?)
+				""";
+		
+		try (
+				Connection con = DriverManager.getConnection(url, username, password);
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				) {
+			
+			pstmt.setString(1, name);
+			pstmt.setDate(2, Date.valueOf(published));
+			pstmt.setInt(3, price);
+			pstmt.setDate(4, Date.valueOf(updated));
+			pstmt.setDouble(5, weight);
+			
+			int cnt = pstmt.executeUpdate();
+			System.out.println(cnt + "개 데이터 추가 완료");
 		}
 		
 	}
