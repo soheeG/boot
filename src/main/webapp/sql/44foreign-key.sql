99f35a7ab500d1204a398b3357d9d33499103654
soheeG <121710859+soheeG@users.noreply.github.com>
2023-04-24 16:31:36 +0900

    20230424

--- src/main/webapp/sql/44foreign-key.sql
+++ src/main/webapp/sql/44foreign-key.sql
@@ -59,3 +59,36 @@ WHERE StudentId = 2;
 DELETE FROM Student
 WHERE Id = 2;
 SELECT * FROM Student;
+
+-- ON : 조인 조건
+SELECT * FROM Products JOIN Categories ON Products.CategoryId = Categories.CategoryId;
+
+-- 조인 후 컬럼 수 : 왼쪽테이블 컬럼 수  + 오른쪽테이블 컬럼 수
+DESC Products; -- 6개 컬럼
+DESC Categories; -- 3개 컬럼
+SELECT * FROM Products JOIN Categories
+ON Products.CategoryID = Categories.CategoryId; -- 9개 컬럼
+
+SELECT ProductName, CategoryName
+FROM Products JOIN Categories
+ON Products.CategoryID = Categories.CategoryId;
+
+SELECT * FROM Products;
+SELECT * FROM Suppliers;
+
+-- 상품명, 그 상품을 공급하는 공급자명
+SELECT ProductName, SupplierName 
+FROM Products JOIN Suppliers
+ON Products .SupplierID = Suppliers.SupplierID
+ORDER BY 2;
+
+-- ALIAS 사용으로 코드 줄이기
+SELECT CustomerName AS Name FROM Customers;
+
+SELECT ProductName, SupplierName
+FROM Products AS p JOIN Suppliers AS s
+ON p.SupplierID = s.SupplierID;
+
+SELECT ProductName, SupplierName
+FROM Products p JOIN Suppliers s
+ON p.SupplierID = s.SupplierID;
---
+++ src/main/webapp/sql/48join.sql
@@ -0,0 +1,16 @@
+DESC Orders;
+SELECT * FROM Orders o JOIN Customers c
+ON o.CustomerID = c.CustomerID; -- 830
+SELECT * FROM Orders o RIGHT JOIN Customers c
+ON o.CustomerID = c.CustomerID; -- 832
+SELECT * FROM Orders o RIGHT JOIN Customers c
+ON o.CustomerID = c.CustomerID
+WHERE o.OrderId IS NULL; -- 주문한적 없는 고객
+
+-- 예) 주문을 처리한 적 없는 직원 조회
+DESC Orders;
+SELECT e.LastName, e.FirstName FROM Orders o RIGHT JOIN Employees e 
+ON o.EmployeeID = e.EmployeeID
+WHERE o.OrderId IS NULL;
+
+SELECT * FROM Orders;
