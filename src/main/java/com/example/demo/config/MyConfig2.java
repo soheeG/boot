ed93777c9a93cd684c005ffd19d5f66a9bfbe3fe
soheeG <121710859+soheeG@users.noreply.github.com>
2023-05-08 15:44:45 +0900

    20230508

--- src/main/java/com/example/demo/config/MyConfig2.java
+++ src/main/java/com/example/demo/config/MyConfig2.java
@@ -1,7 +1,14 @@
 package com.example.demo.config;
 
+import java.util.*;
+
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.userdetails.User.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.provisioning.*;
 import org.springframework.security.web.*;
 
 @Configuration
@@ -10,6 +17,37 @@ public class MyConfig2 {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
+		http.formLogin()
+			.defaultSuccessUrl("/sub33/mylogin")
+			.defaultSuccessUrl("/list", true)
+			.usernameParameter("id") // 기본값 username
+			.passwordParameter("pw"); // 기본값 password
+	
+		http.csrf().disable();
+		
+		http.authorizeHttpRequests().requestMatchers("/abc").authenticated();
+		http.authorizeHttpRequests().anyRequest().permitAll();
+		
 		return http.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		UserDetails user1 = users.username("user1")
+				.password("pw1")
+				.authorities(List.of())
+				.build();
+		UserDetails user2 = users.username("user2")
+				.password("pw2")
+				.authorities(List.of())
+				.build();
+		
+		return new InMemoryUserDetailsManager(user1, user2);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
