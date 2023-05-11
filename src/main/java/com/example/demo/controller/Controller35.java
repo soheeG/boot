89b9d0aee5c144c22c108417ef2805818a426e91
soheeG <121710859+soheeG@users.noreply.github.com>
2023-05-11 11:35:42 +0900

    20230511

--- src/main/java/com/example/demo/controller/Controller35.java
+++ src/main/java/com/example/demo/controller/Controller35.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.*;
+
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+
+import lombok.*;
 
 @Controller
 @RequestMapping("sub35")
@@ -29,7 +33,41 @@ public class Controller35 {
 	}
 	
 	@PostMapping("link4")
-	public void method4(String address) {
+	public void method4(String address, Double price) {
 		System.out.println("address:" + address);
+		System.out.println("price:" + price);
+	}
+	
+	@Data
+	static class Dto1 {
+		private String name;
+		private Double score;
+		private String email;
+	}
+	
+	@PostMapping("link5")
+	public void method5(Dto1 dto) {
+		System.out.println(dto);
+	}
+	
+	@Data
+	static class Dto2 {
+		private String address;
+		private String product;
+		private Double price;
+	}
+	
+	@PostMapping("link6")
+	public void method6(Dto2 dto) {
+		System.out.println(dto);
+	}
+	
+	// json 요청 데이터는 
+	// @RequestBody Map이나 
+	// @RequestBody JavaBean으로 받음
+	@PostMapping("link7")
+	public void method7(@RequestBody Map map) {
+		System.out.println("메소드7 실행됨");
+		System.out.println(map);
 	}
 }
--- src/main/webapp/js/sub35/ajax.js
+++ src/main/webapp/js/sub35/ajax.js
@@ -8,14 +8,36 @@ $("#button2").click(function() {
 
 $("#button3").click(function() {
 	$.ajax("/sub35/link3", {
-		method:"post", 
+		method: "post", 
 		data: "name=손"
 		});
 });
 
 $("#button4").click(function() {
 	$.ajax("/sub35/link4", {
-		method:"post",
-		data:"address=seoul"	
+		method: "post",
+		data: "address=seoul&price=99.99"	
+	});
+});
+
+$("#button5").click(function() {
+	$.ajax("/sub35/link5", {
+		method: "post",
+		data: "name=park&score=8.5&email=park@naver.com"
+	});
+});
+
+$("#button6").click(function() {
+	$.ajax("/sub35/link6", {
+		method: "post",
+		data: "address=jeju&product=fruit&price=99.99"
+	});
+});
+
+$("#button7").click(function() {
+	$.ajax("/sub35/link7", {
+		method: "post",
+		data: '{"name" : "son"}',
+		contentType: "application/json"
 	});
 });
