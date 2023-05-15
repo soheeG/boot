bceed14701bd29ad6bc422d689e25feabb75b034
soheeG <121710859+soheeG@users.noreply.github.com>
2023-05-15 09:13:12 +0900

    20230515

--- src/main/webapp/js/sub39/ajax.js
+++ src/main/webapp/js/sub39/ajax.js
@@ -1,88 +1,88 @@
 $("#btn1").click(function() {
-	$.ajax("/sb39/link1", {
+	$.ajax("/sub39/link1", {
 		success: function() {
 			console.log("성공");
 		},
 		error: function() {
-			console.log("실패");
+			console.log("실패!")
 		},
 		complete: function() {
 			console.log("성공이든 실패든")
 		}
 	});
-});
+})
 
 $("#btn2").click(function() {
-	$.ajax("/sb39/link2", {
+	$.ajax("/sub39/link2", {
 		success: function() {
 			console.log("성공");
 		},
 		error: function() {
-			console.log("실패");
+			console.log("실패!")
 		},
 		complete: function() {
 			console.log("성공이든 실패든")
 		}
 	});
-});
+})
 
 $("#btn3").click(function() {
-	// 성공 응답을 받아서 콘솔에 성공 출력
+	// 성공(200) 응답을 받아서 
+	// 콘솔에 "성공!!!!!!" 출력
 	$.ajax("/sub39/link3", {
 		success: function() {
-			console.log("성공");
+			console.log("성공!!!!!!");
 		}
-	});
+	})
 });
-
 $("#btn4").click(function() {
-	// 실패(500) 응답을 받아서 콘솔에 실패 출력
+	// 실패(500) 응답을 받아서
+	// 콘솔에 "실패!!!!!!!!" 출력
 	$.ajax("/sub39/link4", {
 		error: function() {
-			console.log("실패")
+			console.log("실패!!!!!")
 		}
-	});
-});
+	})
+})
 
 $("#btn5").click(function() {
 	$.ajax("/sub39/link1")
-	.done(function() {
-		console.log("done 메소드 사용, 성공")
-	})
-	.fail(function() {
-		console.log("fail 메소드 사용, 실패")
-	})
-	.always(function() {
-		console.log("always 메소드 사용")
-	});
-});
+		.done(function() {
+			console.log("done 메소드 사용, 성공!!!!!");
+		})
+		.fail(function() {
+			console.log("fail 메소드 사용, 실패!!!!!")
+		})
+		.always(function() {
+			console.log("alwyas 메소드 사용, 성공이든 실패든");
+		});
+})
 
 $("#btn6").click(function() {
 	$.ajax("/sub39/link2")
-	.done(function() {
-		console.log("done 메소드 사용, 성공")
-	})
-	.fail(function() {
-		console.log("fail 메소드 사용, 실패")
-	})
-	.always(function() {
-		console.log("always 메소드 사용")
-	});
-});
+		.done(function() {
+			console.log("done 메소드 사용, 성공!!!!!");
+		})
+		.fail(function() {
+			console.log("fail 메소드 사용, 실패!!!!!")
+		})
+		.always(function() {
+			console.log("alwyas 메소드 사용, 성공이든 실패든");
+		});
+})
 
 $("#btn7").click(function() {
 	$.ajax("/sub39/link3")
-	.done(function() {
-		console.log("성공")
-	})
-});
-
+		.done(function() {
+			console.log("성공!!!!! done")
+		})
+})
 $("#btn8").click(function() {
 	$.ajax("/sub39/link4")
-	.fail(function() {
-		console.log("실패")
-	});
-});
+		.fail(function() {
+			console.log("실패!!!!! fail")
+		})
+})
 
 $("#btn9").click(function() {
 	$.ajax("/sub39/link9", {
@@ -91,72 +91,77 @@ $("#btn9").click(function() {
 			console.log(data);
 			$("#res9").text(data);
 		}
-	});
-});
+	})
+})
 
 $("#btn10").click(function() {
-	// 응답 본문(현재시간)이 콘솔에 출력
+	// 응답본문(현재시간) 이 콘솔에 출력
 	$.ajax("/sub39/link10", {
 		success: function(data) {
 			// console.log(data);
 			$("#res10").text(data);
 		}
-	});
-});
+	}); 
+})
 
 $("#btn11").click(function() {
-	// 응답 본문(현재시간)이 콘솔에 출력
+	// 응답본문(현재시간) 이 콘솔에 출력
 	$.ajax("/sub39/link11", {
 		success: function(data) {
+			// $("#res11").text(data.name);
 			$("#res11").text(data);
 		}
-	});
-});
+	}); 
+})
 
 $("#btn12").click(function() {
 	$.ajax("/sub39/link12", {
-		//content-type : application/json
-		//body : {"address", "gangnam", "city", "seoul", "phone", "iphone"}
+		
+		// content-type : applicaion/json
+		// body : {"address": "seoul", "city":"jeju", "phone":"apple"}
 		success: function(data) {
 			console.log(data.address);
 			console.log(data.city);
-			console.log(data.phone);
+			console.log(data.phone);	
 		}
-	});
-});
+	})
+})
 
 $("#btn13").click(function() {
 	$.ajax("/sub39/link13", {
-		success: function() {
+		success: function(data) {
 			for (let i = 0; i < data.length; i++) {
-				console.log(i);
+				console.log(data[i]);
 			}
 		}
-	});
-});
+	})
+})
 
 $("#btn14").click(function() {
 	const cid = $("#categoryIdInput").val();
+	
 	$.ajax("/sub39/link14?cid=" + cid, {
 		success: function(data) {
+			// console.log(data);
 			$("#res14").empty();
 			for (let i = 0; i < data.length; i++) {
-				$("res14").append(data[i].productName + "<br>");
+				$("#res14").append(data[i].productName + "<br>");
 			}
 		}
-	});
-});
+	})
+})
 
 $("#btn15").click(function() {
 	const cid = $("#categoryIdInput").val();
+	
 	$.ajax("/sub39/link14?cid=" + cid, {
 		success: function(data) {
+			// console.log(data);
 			$("#res15").empty();
-			$("#res15").appemd(`
+			$("#res15").append(`
 				<table class="table">
 					<thead>
 						<tr>
-							<th>
 							<th>
 								상품명
 							</th>
@@ -165,22 +170,23 @@ $("#btn15").click(function() {
 							</th>
 						</tr>
 					</thead>
+					<tbody>
+					</tbody>
 				</table>
 			`);
-			$("#res15 >.table").append("<tbody />");
 			for (let i = 0; i < data.length; i++) {
 				$("#res15 tbody")
-				.append(`
-					<tr>
-						<td>
-							${data[i].productName}
-						</td>
-						<td>
-							${data[i].price}
-						</td>
-					</tr>
-				`);
+					.append(`
+						<tr>
+							<td>
+								${data[i].productName}
+							</td>
+							<td>
+								${data[i].price}
+							</td>
+						</tr>
+					`);
 			}
 		}
-	});
-});
+	})
+})
